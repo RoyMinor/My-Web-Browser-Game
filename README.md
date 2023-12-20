f00b68a8449a10936c65bb3fb0a052401ff1394d
Mandell1981 <Mandell1981.rm@gmail.com>
2023-12-19 20:20:45 -0500

    setting up logic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My-Web-Browser-Game
+<!-- Check List For Game -->
+<!-- write the questions out and answers -->
+<!-- Get Icons for players -->
+<!--  -->
---
+++ index.js
@@ -0,0 +1,49 @@
+// get the elements from html
+// start with an array of ten questions
+let questions = ['What is the planet with a ring ', 'Whats the first planet', ]
+let answers = ['Mercury', 'Saturn', ]
+let mockData = 
+
+// Format for questions
+const QnA = {
+    "QNA1": {
+            "question1": "GME",
+            "answer1": "1day",
+        },
+    "QNA2": {
+            "question2": "GME",
+            "answer2": "1day",
+        },
+    "QNA3": {
+            "question3": "GME",
+            "answer3": "1day",
+        },
+        "QNA4": {
+            "question4": "GME"
+            "answer4": "1day",
+        },
+        "QNA5": {
+            "question5": "GME",
+            "answer5": "1day",
+        },
+        "QNA6": {
+            "question6": "GME",
+            "answer6": "1day",
+        },
+        "QNA7": {
+            "question7": "GME",
+            "answer7": "1day",
+        },
+        "QNA8": {
+            "question8": "GME",
+            "answer8": "1day",
+        },
+        "QNA9": {
+            "question9": "GME",
+            "answser9": "1day",
+        },
+        "QNA10": {
+            "question10": "GME",
+            "answer10": "1day",
+        },
+    }
