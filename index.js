aba18e0fc5c9dad704babbcae55c7e6631e471dd
Mandell1981 <Mandell1981.rm@gmail.com>
2023-12-27 19:46:27 -0500

    updated-score-board

--- index.js
+++ index.js
@@ -39,6 +39,26 @@ const maxQuestions = 10;
 //Question Duration in milliseconds ( 1000 milliseconds = 1second )
 const timerDuration = 10000;
 
+//Score board counter method
+const player1 = {name: "mouse", score: 0};
+const player2 = {name: "cat", score: 0};
+
+function updateScores() {
+    document.getElementById('score1').textContent = `Player 1 Score: ${player1.score}`;
+    document.getElementById('score2').textContent = `Player 2 Score: ${player2.score}`;
+}
+
+document.getElementById('start1').addEventListener('click', function() {
+    player1.name = document.getElementById('playerName1').value;
+    updateScores();
+
+});
+
+document.getElementById('start2').addEventListener('click', function() {
+    player2.name = document.getElementById('playerName2').value;
+    updateScores();
+
+});
 //Scoreboard and question tracker
 let score = 0;
 let askedQuestions = [];
